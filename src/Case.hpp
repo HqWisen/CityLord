f5aef9e36f189d70c644e51cfff206d7d0a16098
David Fishel <dfishel@ulb.ac.be>
2015-02-12 11:57:49 +0100

    [Case.hpp] Case est interface de Road,Obstacle,Field

--- src/Case.hpp
+++ src/Case.hpp
@@ -1,19 +1,22 @@
 #ifndef CASE_HPP_
 #define CASE_HPP_
 
-#include "Obstacle.hpp"
-#include "Field.hpp"
-#include "Road.hpp"
+//#include "Obstacle.hpp"
+//#include "Field.hpp"
+//#include "Road.hpp"
+#include "Location.hpp"
 
 class Case{
-	Obstacle* obs;
-	Field* field;
-	Road* road;
+	//Obstacle* obs;
+	//Field* field;
+	//Road* road;
+	Location coord;
 	public:
-		Case(Obstacle obs);
-		Case(Field field);
-		Case(Road road);
-  		void display();	
+		//Case(Obstacle obs);
+		//Case(Field field);
+		//Case(Road road);
+  		void display();
+  		Location getCoord();
 };
 
 #endif // CASE_HPP_
