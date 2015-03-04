85e357b7b12ce3fa8116ecb830647d70f6a2866c
Kaio Lopes <klopes@ulb.ac.be>
2015-03-04 13:14:40 +0100

    Changement dans updateBuildings et makeVisitorsAdvance

--- src/server/CityManager.cpp
+++ src/server/CityManager.cpp
@@ -236,10 +236,31 @@ void CityManager::updateCity(){
 }
 
 void CityManager::updateBuildings(){
+	Location currentLocation;
+	for(int i = 0; i<cityMap.getDimensionX(); i++){
+		for(int j = 0; i<cityMap.getDimensionY(); j++){
+			currentLocation = Location(x,y);
+			Field* concernedField = dynamic_cast<Field*>(cityMap.getCase(currentLocation))
+			if(concernedField->hasBuilding()){;
+				//Faire sortir les visiteurs du building, puis faire payer le joueur
+			}
+		}
+	}
 }
 
-void CityManager::makeVisitorsAvance(){
-	
+void CityManager::makeVisitorsAdvance(){
+	Location currentLocation;
+	for(int i = 0; i<cityMap.getDimensionX(); i++){
+		for(int j = 0; i<cityMap.getDimensionY(); j++){
+			currentLocation = Location(x,y);
+			Case* concernedCase = cityMap.getCase(currentLocation)
+			if(concernedCase->hasVisitors()){
+				for(k = 0; k <concernedCase->visitorList.size(); k++){
+					concernedCase->visitorList->moveGUI();
+				}
+			}
+		}
+	}
 }
 
 void CityManager::spawnVisitors(){
