ad4e77e41ed5cae4d29b3f65f2e128e567bc0f1f
Kaio Lopes <klopes@ulb.ac.be>
2015-03-05 13:54:26 +0100

    Déplacement des fonctions de temps de CityManager à CityUpdater

--- src/server/CityManager.cpp
+++ src/server/CityManager.cpp
@@ -227,49 +227,4 @@ std::string showCity(){
 	return cityMap.display();
 }
 
-
-void CityManager::updateCity(){
-	if((Timer.elapsedTime() - lastUpdateTime)>=UPDATE_INTERVAL){
-		spawnVisitors();
-		updateBuildings();
-		makeVisitorsAdvance();
-}
-
-void CityManager::updateBuildings(){
-	Location currentLocation;
-	for(int i = 0; i<cityMap.getDimensionX(); i++){
-		for(int j = 0; i<cityMap.getDimensionY(); j++){
-			currentLocation = Location(x,y);
-			Field* concernedField = dynamic_cast<Field*>(cityMap.getCase(currentLocation))
-			if(concernedField->hasBuilding()){;
-				//Faire sortir les visiteurs du building, puis faire payer le joueur
-			}
-		}
-	}
-}
-
-void CityManager::makeVisitorsAdvance(){
-	Location currentLocation;
-	for(int i = 0; i<cityMap.getDimensionX(); i++){
-		for(int j = 0; i<cityMap.getDimensionY(); j++){
-			currentLocation = Location(x,y);
-			Case* concernedCase = cityMap.getCase(currentLocation)
-			if(concernedCase->hasVisitors()){
-				for(k = 0; k <concernedCase->visitorList.size(); k++){
-					concernedCase->visitorList->moveGUI();
-				}
-			}
-		}
-	}
-}
-
-void CityManager::spawnVisitors(){
-	
-}
-void CityManager::run(){
-	while(1){
-		updateCity();
-	}
-}
-
 */
--- src/server/CityUpdater.cpp
+++ src/server/CityUpdater.cpp
@@ -34,9 +34,29 @@ void cityUpdater::generateVisitor(){
 }
 
 void cityUpdater::updateBuildings(){
-
+	/*Location currentLocation;
+	for(int i = 0; i<cityMap.getDimensionX(); i++){
+		for(int j = 0; i<cityMap.getDimensionY(); j++){
+			currentLocation = Location(x,y);
+			Field* concernedField = dynamic_cast<Field*>(cityMap.getCase(currentLocation))
+			if(concernedField->hasBuilding()){;
+				//Faire sortir les visiteurs du building, puis faire payer le joueur
+			}
+		}
+	}*/
 }
 
 void cityUpdater::makeVisitorsAdvance(){
-	
+	/*Location currentLocation;
+	for(int i = 0; i<cityMap.getDimensionX(); i++){
+		for(int j = 0; i<cityMap.getDimensionY(); j++){
+			currentLocation = Location(x,y);
+			Case* concernedCase = cityMap.getCase(currentLocation)
+			if(concernedCase->hasVisitors()){
+				for(k = 0; k <concernedCase->visitorList.size(); k++){
+					concernedCase->visitorList->moveGUI();
+				}
+			}
+		}
+	}*/
 }
