2e04803e03f138b0ce8054d97f4284db5cede4cd
David Fishel <dfishel@ulb.ac.be>
2015-04-05 18:40:40 +0200

    [sell] fenetre de vente lorsqu'on clique sur Sell, pas pu être testé, du à Timer

---
+++ src/client/gui/sell.h
@@ -0,0 +1,38 @@
+#ifndef SELL_H
+#define SELL_H
+
+#include <QWidget>
+#include <QDialog>
+#include <QTableWidgetItem>
+#include <QMessageBox>
+#include "clientmanagerGUI.h"
+
+namespace Ui {
+class Sell;
+}
+
+class Sell : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Sell(QWidget*, ClientManagerGUI*);
+    ~Sell();
+    void refresh();
+    QPushButton* getCancelButton();
+private slots:
+    void on_putOnDialogButton_clicked();
+
+    void on_takeOffDialogButton_clicked();
+
+    void on_cancelButton_clicked();
+
+private:
+    Ui::Sell *ui;
+    ClientManagerGUI* clientManager;
+    QTableWidgetItem*** tableItemArray;
+    QStringList headerLabels;
+    int choice;
+};
+
+#endif // SELL_H
