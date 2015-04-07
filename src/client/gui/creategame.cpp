06931163d432261527ce76111dd4948302f3b9dc
Gabriel Ortega <gortega@ulb.ac.be>
2015-04-08 01:22:45 +0200

    Integration de gamemode dans le GUI (merci pour le bouton david :D)

--- src/client/gui/creategame.cpp
+++ src/client/gui/creategame.cpp
@@ -10,7 +10,7 @@ const std::string CreateGame::UNCLICKCOLOR = "black";
 
 CreateGame::CreateGame(QWidget* parent, ClientManagerGUI* cm) :
     DefaultWidget(parent, cm), ui(new Ui::CreateGame){
-    gameMode = "Normal";
+    gameMode = 1;
     displayDefaultBackground();
     ui->setupUi(this);
     refresh();
@@ -57,7 +57,8 @@ void CreateGame::on_map3Button_clicked(){
 
 void CreateGame::on_createButton_clicked(){
     clientManager->setRequest("createcity");
-    clientManager->addInfo("number", std::to_string(choice));
+    clientManager->addInfo("mapnumber", std::to_string(choice));
+    clientManager->addInfo("modenumber", std::to_string(gameMode));
     clientManager->sendRequestAndRecv();
     if(clientManager->requestFailed()){
         // TODO handle failure
@@ -90,11 +91,15 @@ void CreateGame::on_gameModeSelect_currentIndexChanged(int index)
 void CreateGame::changedGameMode(){
     std::string selected=ui->gameModeSelect->currentText().toStdString();
     if(selected=="Normal"){
-        gameMode = "Normal";
-        ui->gameModeLabel->setText("Normal building price, etc");
+        gameMode = 1;
+        ui->gameModeLabel->setText("Normal prices, normal income etc");
+    }
+    else if(selected=="Easy"){
+        gameMode = 2;
+        ui->gameModeLabel->setText("Lower prices, higher income, etc");
     }
     else if(selected=="Hardcore"){
-        gameMode = "Hardcore";
-        ui->gameModeLabel->setText("Higher difficulty, prices, etc");
+        gameMode = 3;
+        ui->gameModeLabel->setText("Higher prices, lower income, etc");
     }
 }
